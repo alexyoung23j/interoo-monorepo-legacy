7b32fe2c63e20bd78222e1389ac0db51dd2717c1
Alex Young <alexyoung23j@gmail.com>
2024-08-24 20:35:57 -0400

    works lfg

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,11 @@
-import express, { Request, Response } from 'express';
-import { PrismaClient } from '../../shared/generated/client';
+import express, { Request, Response } from "express";
+import { PrismaClient } from "../../shared/generated/client";
+import dotenv from "dotenv";
+import path from "path";
+
+// Get __dirname equivalent in ES modules
+const rootDir = path.resolve(__dirname, "../..");
+dotenv.config({ path: path.join(rootDir, ".env") });
 
 const app = express();
 const port = 3000;
@@ -9,7 +15,7 @@ const prisma = new PrismaClient();
 app.use(express.json());
 
 // Basic route
-app.get('/', async (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   const posts = await prisma.post.findFirst();
   res.send(`Hello World! First user: ${JSON.stringify(posts)}`);
 });
