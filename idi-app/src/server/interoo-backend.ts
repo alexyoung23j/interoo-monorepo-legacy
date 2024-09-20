c801e4cd93de95fa113b6569b384122b3eec0c58
Ronit Tiwary <tiwaryronit@gmail.com>
2024-09-19 20:51:35 -0400

    use mediaUrl

--- idi-app/src/server/interoo-backend.ts
+++ idi-app/src/server/interoo-backend.ts
@@ -2,20 +2,14 @@ import axios from "axios";
 
 export const fetchResponses = async ({
   responseIds,
-  studyId,
-  questionId,
-  orgId,
   token,
 }: {
   responseIds: string[];
-  studyId: string;
-  questionId: string;
-  orgId: string;
   token: string;
 }) => {
   const res = await axios.post(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/get-signed-urls-for-media-view/batch`,
-    { responseIds, studyId, questionId, orgId },
+    { responseIds },
     {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -27,31 +21,19 @@ export const fetchResponses = async ({
 };
 
 export const downloadMedia = async ({
-  url,
   targetFormat,
   responseId,
-  orgId,
-  studyId,
-  questionId,
   token,
 }: {
-  url: string;
   targetFormat: string;
   responseId: string;
-  orgId: string;
-  studyId: string;
-  questionId: string;
   token: string;
 }) => {
   const response = await axios.post(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/convert-and-download`,
     {
-      url,
       targetFormat,
       responseId,
-      orgId,
-      studyId,
-      questionId,
     },
     {
       headers: {
