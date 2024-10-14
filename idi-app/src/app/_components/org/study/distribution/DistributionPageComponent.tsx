20695ed7fc967311b036dc97f468e68a753ba149
Alex Young (Interoo) <alex@interoo.ai>
2024-10-13 20:48:57 -0400

    cleanup

--- idi-app/src/app/_components/org/study/distribution/DistributionPageComponent.tsx
+++ idi-app/src/app/_components/org/study/distribution/DistributionPageComponent.tsx
@@ -26,34 +26,6 @@ const DistributionPageComponent: React.FC<DistributionPageComponentProps> = ({
     },
   );
 
-  const [maxParticipants, setMaxParticipants] = useState<number | null>(null);
-  const [hasChangedMaxParticipants, setHasChangedMaxParticipants] =
-    useState(false);
-
-  const updateStudy = api.studies.updateStudy.useMutation();
-
-  useEffect(() => {
-    if (study?.maxResponses) {
-      setMaxParticipants(study.maxResponses);
-    }
-  }, [study]);
-
-  const handleSave = async () => {
-    try {
-      if (maxParticipants) {
-        setHasChangedMaxParticipants(false);
-        await updateStudy.mutateAsync({
-          id: study?.id ?? "",
-          maxResponses: maxParticipants,
-        });
-        showSuccessToast("Max participants updated.");
-      }
-    } catch (error) {
-      console.error(error);
-      showErrorToast("Failed to update study.");
-    }
-  };
-
   if (isLoading || !study) {
     return (
       <div className="flex h-full items-center justify-center bg-theme-off-white">
@@ -72,33 +44,6 @@ const DistributionPageComponent: React.FC<DistributionPageComponentProps> = ({
           <BasicLinkCopy
             link={`${window.location.origin}/study/${study?.shortID}`}
           />
-        </div>
-      </BasicTitleSection>
-      <BasicTitleSection
-        title="Maximum Participants"
-        subtitle="The survey will no longer accept responses after this
-        number has been reached."
-      >
-        <div className="flex max-w-72 items-center gap-4">
-          <BasicInput
-            type="number"
-            placeholder="enter number"
-            value={maxParticipants ?? ""}
-            onSetValue={(value) => {
-              const parsedValue = parseInt(value);
-              setMaxParticipants(isNaN(parsedValue) ? null : parsedValue);
-              setHasChangedMaxParticipants(true);
-            }}
-            disabled={study?.status === StudyStatus.PUBLISHED}
-          />
-          <Button
-            size="sm"
-            className="text-theme-off-white"
-            disabled={!hasChangedMaxParticipants}
-            onClick={handleSave}
-          >
-            Save
-          </Button>
         </div>
       </BasicTitleSection>
     </div>
